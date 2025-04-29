698aff2bdb59e0d824f55e494a12efb484f7d31b
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-04-29 11:04:51 +0700

    menambahkan fungsi

--- Introduce.cpp
+++ Introduce.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
 
-
+class mahasiswa{
 public:
 int nim;
 string nama;
 float nilai;
+
+void print_data(){
+    cout << "NIM = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+    cout << "Nilai = " << nilai << endl;
+}
+};
 
 int main(){
